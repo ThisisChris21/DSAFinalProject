eb2067d8d19de5616a33f3d991dfef7d5c46ca5d
Clarence Christopher Lauron <clarencechristopherlauron@gmail.com>
2025-01-25 15:54:26 +0800

    g ui

--- index.js
+++ index.js
@@ -4,9 +4,43 @@ import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
 import bookRoute from './routes/booksRoute.js';
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(express.json());
+
+
+//middleware for handling CORS policy
+//===================================
+
+//option 1: Allow all origins with default of cors(*)
+app.use(cors());
+//option 2: Allow cusotm origins
+/*
+app.use({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-type'],
+});
+*/
+app.use('/books', bookRoute);
+
+//middleware communicate with booksRoute.js
+
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "dist"))); // Adjust path if necessary
+
+// Catch-all handler to serve React's index.html for unmatched routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 
 
 mongoose
@@ -23,35 +57,3 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
-
-//middleware for handling CORS policy
-//===================================
-
-//option 1: Allow all origins with default of cors(*)
-app.use(cors());
-//option 2: Allow cusotm origins
-/*
-app.use({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-type'],
-});
-*/
-
-//middleware communicate with booksRoute.js
-
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "dist"))); // Adjust path if necessary
-
-// Catch-all handler to serve React's index.html for unmatched routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
-
-app.use('/books', bookRoute);
